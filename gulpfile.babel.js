4edcfec86994bef6dd4f5c1d0c32ab9b0ebd3709
jerometrie <jerometrie@yahoo.com>
2016-06-11 00:17:14 +0200

    Complete the watch and sync tasks in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,33 +4,54 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
-let sass = require("gulp-sass");
+let sass = require("gulp-sass"),
+	sync = require("browser-sync");
 
 /********************************************
 /	DEVELOPMENT TASKS	src/ -> .tmp/
 /*******************************************/
 
 
-// Transpile and bundle JS files into one single bundle.js
+// Transpile and bundle JS files into one single bundle.js, and refresh browser
 gulp.task("bundlejs", function() {
 	return browserify("src/js/app.js")
 		.transform("babelify")
 		.bundle()
 		.pipe(source("bundle.js"))
-		.pipe(gulp.dest(".tmp/js"));
+		.pipe(gulp.dest(".tmp/js"))
+		.pipe(sync.reload({
+			stream: true
+		}));
 });
 
-// Compile all scss files into css
+// Compile all scss files into css and refresh browser
 gulp.task("sass", function() {
 	return gulp.src("src/scss/**/*.scss")
 		.pipe(sass())
-		.pipe(gulp.dest(".tmp/css"));
+		.pipe(gulp.dest(".tmp/css"))
+		.pipe(sync.reload({
+			stream: true
+		}));
 });
 
 // Watch task
-gulp.task('watch', function() {
+// Before it's started, we run sync, sass, ans bundlejs
+gulp.task("watch", ["sync", "sass", "bundlejs"], function() {
 	// Watch all scss files in src/scss, then execute "sass"
 	gulp.watch("src/scss/**/*.scss", ["sass"]);
 	// Watch all js files in src/js, then execute "bundlejs"
 	gulp.watch("src/js/**/*.js", ["bundlejs"]);
 });
+
+// Initialization of browser sync by showing what is in .tmp
+gulp.task("sync", function() {
+	sync.init({
+		server: {
+			baseDir: ".tmp"
+		},
+		browser: "google chrome"
+	});
+});
+
+
+
