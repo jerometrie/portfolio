6fc779810aed382c197c941d16d3810e04767a58
jerometrie <jerometrie@yahoo.com>
2016-06-18 13:53:57 +0200

    Fixes for menu

--- .tmp/js/bundle.js
+++ .tmp/js/bundle.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 	// Take care of the menu handle
 	$(".handle").on("click", function (event) {
 		event.preventDefault();
-		$(".modal").toggleClass("showing");
+		$(".js-modal").toggleClass("showing");
 	});
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
 	// Take care of the menu handle
 	$(".handle").on("click", function(event) {
 		event.preventDefault();
-		$(".modal").toggleClass("showing");
+		$(".js-modal").toggleClass("showing");
 	});
 });
